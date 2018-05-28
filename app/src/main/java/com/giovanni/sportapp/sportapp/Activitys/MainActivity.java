87f6bd96658dd66bebdbf0acc86249f8120e5bc4
giovannicortiano <giovannicortiano@hotmail.com>
2018-05-28 00:16:02 -0300

    RQ02 - Criação do actionBar principal

--- app/src/main/java/com/giovanni/sportapp/sportapp/Activitys/MainActivity.java
+++ app/src/main/java/com/giovanni/sportapp/sportapp/Activitys/MainActivity.java
@@ -3,6 +3,10 @@ package com.giovanni.sportapp.sportapp.Activitys;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,20 +19,31 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSair;
+    private Toolbar ToolBarPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSair = findViewById(R.id.btnSair);
-        btnSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseUser user = ConfiguradorFireBase.getAutenticadorFireBase().getCurrentUser();
-                ConfiguradorFireBase.getAutenticadorFireBase().signOut();
-                finish();
-            }
-        });
+        ToolBarPrincipal = findViewById(R.id.toolBarPrincipal);
+        ToolBarPrincipal.setTitle("Sportapp");
+        setSupportActionBar(ToolBarPrincipal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuSair){
+            ConfiguradorFireBase.getAutenticadorFireBase().signOut();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
