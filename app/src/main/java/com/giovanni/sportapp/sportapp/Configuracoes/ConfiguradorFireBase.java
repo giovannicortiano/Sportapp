dd57366db8999e34e81e96306ef58fd554dd74ff
giovannicortiano <giovannicortiano@hotmail.com>
2018-06-02 00:29:29 -0300

    Melhorias nas telas já existentes

--- app/src/main/java/com/giovanni/sportapp/sportapp/Configuracoes/ConfiguradorFireBase.java
+++ app/src/main/java/com/giovanni/sportapp/sportapp/Configuracoes/ConfiguradorFireBase.java
@@ -1,14 +1,16 @@
 package com.giovanni.sportapp.sportapp.Configuracoes;
 
-import android.support.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfiguradorFireBase {
     private static DatabaseReference BancoDeDadosFireBase;
     private static FirebaseAuth AutenticadorFireBase;
+    private static StorageReference ArmazenadorDeImagensFireBase;
 
     public static DatabaseReference getBancoDeDadosFireBase(){
         if (BancoDeDadosFireBase == null) {
@@ -23,5 +25,12 @@ public class ConfiguradorFireBase {
         }
 
         return AutenticadorFireBase;
+    }
+
+    public static StorageReference getArmazenadorDeImagensFireBase(){
+        if (ArmazenadorDeImagensFireBase == null){
+            ArmazenadorDeImagensFireBase = FirebaseStorage.getInstance().getReference();
+        }
+        return ArmazenadorDeImagensFireBase;
     }
 }
